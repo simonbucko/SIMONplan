c3c001dc60cf5c3896fbe8ac65544b5d1b36e72a
simonbucko <38184399+simonbucko@users.noreply.github.com>
2020-07-11 09:47:58 +0200

    Updates

---
+++ precache-manifest.81fe8897a3037cbb17bd78107f3c10bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695550feeb59d8a34132f8a8306c37ae",
+    "url": "/SIMONplan/index.html"
+  },
+  {
+    "revision": "1bb40ecd96e420d6283f",
+    "url": "/SIMONplan/static/css/main.d8de464a.chunk.css"
+  },
+  {
+    "revision": "421400bcf41f58653031",
+    "url": "/SIMONplan/static/js/2.daf56afb.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/SIMONplan/static/js/2.daf56afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb40ecd96e420d6283f",
+    "url": "/SIMONplan/static/js/main.a4c9468c.chunk.js"
+  },
+  {
+    "revision": "e16c3dc98f3ca58fa8a0",
+    "url": "/SIMONplan/static/js/runtime-main.10cbb8fa.js"
+  }
+]);
